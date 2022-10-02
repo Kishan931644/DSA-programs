4ead0589b808211e4e77c8846611b0b222373f8a
kishan93273110 <techno931644@gmail.com>
2022-10-02 15:54:39 +0530

    Program 1 in complete

--- max_min_in_array.cpp
+++ max_min_in_array.cpp
@@ -1,8 +1,28 @@
 // Maximum and minimum of an array using minimum number of comparisons
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    
+int main(int argc, char const *argv[])
+{
+    int arr[] = {10, 20, 40, 60};
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+
+    cout << "MAX is: " << max;
+    cout << endl;
+    cout << "MIN is: " << min;
+
     return 0;
 }
